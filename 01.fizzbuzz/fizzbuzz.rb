92198f96152e6cb6a8dcce860f1542c55dc2dee0
HaGi-KN <kento.hagihara.1@gmail.com>
2024-01-25 17:27:28 +0900

    Modified from if to case

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,8 +1,7 @@
-(1..20).each do |x|
-  case 
-  when x % 3 == 0 && x % 5 == 0 then puts "FizzBuzz"  
-  when x % 3 == 0 then puts "Fizz"
-  when x % 5 == 0 then puts "Buzz"
+(1..20).each do |x| 
+  if x % 3 == 0 && x % 5 == 0 then puts "FizzBuzz"  
+  elsif x % 3 == 0 then puts "Fizz"
+  elsif x % 5 == 0 then puts "Buzz"
   else puts x
   end
 end
