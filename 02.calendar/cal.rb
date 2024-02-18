56b34c21d2946539c3edde60adf3141820a73643
HaGi-KN <kento.hagihara.1@gmail.com>
2024-02-18 23:08:34 +0900

    Modified conditionals

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -10,29 +10,24 @@ params_year = params["y"]||today_year
 puts "#{params_mon}月\s#{params_year}".center(20)
 puts "日\s月\s火\s水\s木\s金\s土\n"
 
-fast_day = Date.new(params_year.to_i, params_mon.to_i, +1)
+first_day = Date.new(params_year.to_i, params_mon.to_i, +1)
 last_day = Date.new(params_year.to_i, params_mon.to_i, -1)
 
-(fast_day..last_day).each do |x| 
-  if x == fast_day && x == Date.today    
-    wday = x.wday
-    wday.times {print "\s\s\s"}
-    print "\e[7m#{fast_day.day.to_s.rjust(2)}\e[0m\s" 
-  elsif x == fast_day    
-    wday = x.wday
-    wday.times {print "\s\s\s"}
-    print fast_day.day.to_s.center(3)
-  elsif x.saturday? && x == Date.today
-    puts "\e[7m#{x.day.to_s.rjust(2)}\e[0m\s" 
-  elsif x.saturday?
-    puts x.day.to_s.center(3)
-  elsif x == last_day && x == Date.today 
-    puts "\e[7m#{x.day.to_s.rjust(2)}\e[0m\s"
-  elsif x == last_day 
-    puts x.day.to_s.center(3)
-  elsif x == Date.today && x == Date.today
-    print "\e[7m#{x.day.to_s.rjust(2)}\e[0m\s"
-  else x == Date.today
-    print x.day.to_s.center(3)
+first_day.wday.times {print "\s\s\s"}
+
+(first_day..last_day).each do |x| 
+  day_center = x.day.to_s.center(3) 
+  saturday_rjust = x.day.to_s.rjust(2) 
+
+  if x == Date.today
+    print "\e[7m#{saturday_rjust}\e[0m\s"
+  else
+    print day_center
+  end
+  
+  if x.saturday?
+    print "\n"
   end
 end
+
+puts "\n" 
